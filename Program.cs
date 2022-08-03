2118e96463f6868b69ed7b260bb061a888b62292
Kevin Foughty <97193605+metalBread33@users.noreply.github.com>
2022-08-03 00:36:38 -0400

    Add project files.

---
+++ Program.cs
@@ -0,0 +1,12 @@
+﻿using System;
+
+namespace Libary
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Hello World!");
+        }
+    }
+}
